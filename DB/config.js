99251b4258466922edd51ab04d147b8a9fe288e5
shagorpixel <shagorpixel@gmail.com>
2025-05-19 19:49:56 +0600

    Environment Variable Added

--- DB/config.js
+++ DB/config.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const DB_URL = process.env.DB_URL;
-const port = 3000;
+const DB_URL = process.env.DB_URL;
+const port = process.env.PORT;
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://shagormta0:Fc8zbem7Q7LIgjL9@cluster0.pb5ucuy.mongodb.net/todo");
+        await mongoose.connect(DB_URL);
         console.log('Database Connected Successfully');
     }catch(error){
         console.log(error.message);
